11c8a7817c009988fa17716adebb758ed8ecc02f
gwct <sonotiro@gmail.com>
2015-08-19 13:24:53 -0400

    fa_edit now can replace or add to headers; can replace multiple residues at a time

--- fa_edit.py
+++ fa_edit.py
@@ -39,7 +39,7 @@ import core
 #MULTIZ 100 SPEC ALIGN
 #"hg19:hg19,panTro4:panTro4,gorGor3:gorGor3,ponAbe2:ponAbe2,nomLeu3:nomLeu3,rheMac3:rheMac3,macFas5:macFas5,papHam1:papHam1,chlSab1:chlSab1,calJac3:calJac3,saiBol1:saiBol1,otoGar3:otoGar3,tupChi1:tupChi1,speTri2:speTri2,jacJac1:jacJac1,micOch1:micOch1,criGri1:criGri1,mesAur1:mesAur1,mm10:mm10,rn5:rn5,hetGla2:hetGla2,cavPor3:cavPor3,chiLan1:chiLan1,octDeg1:octDeg1,oryCun2:oryCun2,ochPri3:ochPri3,susScr3:susScr3,vicPac2:vicPac2,camFer1:camFer1,turTru2:turTru2,orcOrc1:orcOrc1,panHod1:panHod1,bosTau7:bosTau7,oviAri3:oviAri3,capHir1:capHir1,equCab2:equCab2,cerSim1:cerSim1,felCat5:felCat5,canFam3:canFam3,musFur1:musFur1,ailMel1:ailMel1,odoRosDiv1:odoRosDiv1,lepWed1:lepWed1,pteAle1:pteAle1,pteVam1:pteVam1,myoDav1:myoDav1,myoLuc2:myoLuc2,eptFus1:eptFus1,eriEur2:eriEur2,sorAra2:sorAra2,conCri1:conCri1,loxAfr3:loxAfr3,eleEdw1:eleEdw1,triMan1:triMan1,chrAsi1:chrAsi1,echTel2:echTel2,oryAfe1:oryAfe1,dasNov3:dasNov3,monDom5:monDom5,sarHar1:sarHar1,macEug2:macEug2,ornAna1:ornAna1,falChe1:falChe1,falPer1:falPer1,ficAlb2:ficAlb2,zonAlb1:zonAlb1,geoFor1:geoFor1,taeGut2:taeGut2,pseHum1:pseHum1,melUnd1:melUnd1,amaVit1:amaVit1,araMac1:araMac1,colLiv1:colLiv1,anaPla1:anaPla1,galGal4:galGal4,melGal1:melGal1,allMis1:allMis1,cheMyd1:cheMyd1,chrPic1:chrPic1,pelSin1:pelSin1,apaSpi1:apaSpi1,anoCar2:anoCar2,xenTro7:xenTro7,latCha1:latCha1,tetNig2:tetNig2,fr3:fr3,takFla1:takFla1,oreNil2:oreNil2,neoBri1:neoBri1,hapBur1:hapBur1,mayZeb1:mayZeb1,punNye1:punNye1,oryLat2:oryLat2,xipMac1:xipMac1,gasAcu1:gasAcu1,gadMor1:gadMor1,danRer7:danRer7,astMex1:astMex1,lepOcu1:lepOcu1,petMar2:petMar2"
 
-aas = ["G","A","V","L","I","P","F","Y","W","S","T","C","M","N","Q","K","R","H","D","E", "X"];
+aas = ["G","A","V","L","I","P","F","Y","W","S","T","C","M","N","Q","K","R","H","D","E","X"];
 nts = ["A","T","C","G"];
 
 ############################################
@@ -51,12 +51,12 @@ def IO_fileParse():
 	parser = argparse.ArgumentParser(description="A general purpose FASTA editing script.");
 
 	parser.add_argument("-i", dest="input", help="A directory containing FASTA formatted files or a single FASTA file.");
-	parser.add_argument("-r", dest="relabel_opt", help="Boolean to tell the script whether to relabel the FASTA headers (1) or not (0). Default: 1", type=int, default=1);
+	parser.add_argument("-r", dest="relabel_opt", help="Option to tell the script whether to relabel the FASTA headers (1,2,3) or not (0). 1: Replace header completely. 2: Add new header to beginning of old header. 3: i5k. Default: 1", type=int, default=1);
 	parser.add_argument("-s", dest="spec_dict", help="A string formatted as a Python dictionary with the current species ID as the key and the label to add to the beginning of the FASTA header as the value. Must be provided if -r set to 1.");
 	parser.add_argument("-t", dest="trim_opt", help="Boolean to tell the script whether to trim the FASTA headers (1) or not (0). Default: 0", type=int, default=0);
 	parser.add_argument("-d", dest="trim_delim", help="The character string at which to trim the FASTA headers if -t is set to 1. Default: \" \"", default=" ");
 	parser.add_argument("-p", dest="ss_opt", help="Boolean to tell the script whether to remove start and stops from the alignment (1) or not (0). Default: 0", type=int, default=0);
-	parser.add_argument("-m", dest="replacement", help="This option will replace all characters in each sequence with another character. For example, AB will replace all As with Bs. If the input is an alignment, if A: is entered, all As will be replaced with another AA that is not present in the column.", default="");
+	parser.add_argument("-m", dest="replacement", help="This option will replace all characters in each sequence with another character. For example, AB will replace all As with Bs. If the input is an alignment, if A: is entered, all As will be replaced with another AA that is not present in the column. For multiple replacements, enter as: AB,CD,EF", default="");
 	parser.add_argument("-o", dest="output", help="The directory or file to which the relabeled and/or trimmed FASTA sequences are written.");
 
 	args = parser.parse_args();
@@ -66,9 +66,9 @@ def IO_fileParse():
 		sys.exit();
 
 	if args.relabel_opt not in [0,1]:
-		print " ------------------------------------------------";
-		print "|**Error 1: -r must take values of either 0 or 1 |";
-		print " ------------------------------------------------";
+		print " -------------------------------------------------------";
+		print "|**Error 1: -r must take values of either 0, 1, 2, or 3 |";
+		print " -------------------------------------------------------";
 		parser.print_help();
 		sys.exit();
 	elif args.relabel_opt == 1:
@@ -105,12 +105,26 @@ def IO_fileParse():
 	if args.replacement != "":
 		replacement = replacement.upper();
 
-		if len(replacement) > 2 or (replacement[1] not in aas and replacement[1] != ":"):
-			print " -----------------------------------------------------------------------------------------";
-			print "|**Error 5: For -m, the second character entered must be a valid amino acid symbol or ':' |";
-			print " -----------------------------------------------------------------------------------------";
+		if len(replacement) > 2 and replacement.find(",") == -1:
+			print " -----------------------------------";
+			print "|**Error 5: -m entered incorrectly. |";
+			print " -----------------------------------";
 			parser.print_help();
 			sys.exit();
+
+		elif replacement.find(",") != -1:
+			replacement = replacement.split(",");
+
+		else:
+			replacement = [replacement];
+
+		for each in replacement:
+			if len(each) > 2 or (each[1] not in aas and each[1] != ":"):
+				print " -----------------------------------------------------------------------------------------";
+				print "|**Error 5: For -m, the second character entered must be a valid amino acid symbol or ':' |";
+				print " -----------------------------------------------------------------------------------------";
+				parser.print_help();
+				sys.exit();
 
 
 	return args.input, args.relabel_opt, sd, args.trim_opt, args.trim_delim, args.ss_opt, replacement, args.output;
@@ -135,7 +149,7 @@ if r == 0 and t == 0 and ss == 0 and repl == "":
 	print "INFO     | NOT trimming OR relabeling FASTA headers OR removing start/stop positions.\nSimply re-writing the sequences.";
 	suffix = ".cp";
 else:
-	if r == 1:
+	if r >= 1:
 		print "---";
 		print "INFO     | Relabeling FASTA header(s).";
 		print "EXISTING LABEL\t\tADDED LABEL";
@@ -158,11 +172,12 @@ else:
 		suffix = suffix + ".ss";
 	else:
 		print "INFO     | NOT removing start and stop positions.";
-	if repl != "" and repl[1] != ":":
-		print "INFO     | Replacing all " + repl[0] + " symbols in input sequences with " + repl[1] + ".";
-		suffix = suffix + ".r";
-	elif repl != "" and repl[1] == ":":
-		print "INFO     | Replacing all " + repl[0] + " symbols in input sequences with a random AA symbol not present in that column.";
+	if repl != None:
+		for rep in repl:
+			if rep != "" and rep[1] != ":":
+				print "INFO     | Replacing all " + rep[0] + " symbols in input sequences with " + rep[1] + ".";
+			elif rep != "" and rep[1] == ":":
+				print "INFO     | Replacing all " + rep[0] + " symbols in input sequences with a random AA symbol not present in that column.";
 		suffix = suffix + ".r";
 if os.path.isfile(ins):
 	print "OUTPUT   | Writing output to the following file:\t\t" + outs;
@@ -205,14 +220,20 @@ for each in filelist:
 	inseqs = core.fastaGetDict(infilename);
 	#Reads the sequences from the input file.
 
-	if r == 1:
+	if r >= 1:
 	#Re-labels the FASTA headers.
 		rseqs = {};
 		for title in inseqs:
-			curid = title[1:title.index(":")];
+			if r == 3:
+				curid = title[1:title.index(":")];
+			else:
+				curid = title[1:];
 			for specid in specdict:
 				if curid.find(specid) != -1:
-					newtitle = ">" + specdict[specid] + " " + title[1:];
+					if r == 1:
+						newtitle = ">" + specdict[specid];
+					elif r == 2 or 3:
+						newtitle = ">" + specdict[specid] + " " + title[1:];	
 					rseqs[newtitle] = inseqs[title];
 					break;
 		inseqs = rseqs;
@@ -235,36 +256,37 @@ for each in filelist:
 			if inseqs[title].find("*") != -1:
 				inseqs[title] = inseqs[title].replace("*","");
 
-	if repl != "":
+	if repl != None:
 	#Replaces symbols in the sequences.
-		r1 = repl[0];
-		r2 = repl[1];
 		seqlen = len(inseqs[inseqs.keys()[0]]);
-		if r2 == ":":
-			pseqs = {};
-			for x in xrange(0,seqlen):
-				present = [];
-				for title in inseqs:
-					if x == 0:
-						pseqs[title] = "";
-					if inseqs[title][x] not in present:
-						present.append(inseqs[title][x]);
-				if r1 in present:
-					newaa = present[0];
-					while newaa in present:
-						newaa = random.choice(aas);
+		for rep in repl:
+			r1 = rep[0];
+			r2 = rep[1];
+			if r2 == ":":
+				pseqs = {};
+				for x in xrange(0,seqlen):
+					present = [];
 					for title in inseqs:
-						if inseqs[title][x] == r1:
-							pseqs[title] = pseqs[title] + newaa;
-						else:
+						if x == 0:
+							pseqs[title] = "";
+						if inseqs[title][x] not in present:
+							present.append(inseqs[title][x]);
+					if r1 in present:
+						newaa = present[0];
+						while newaa in present:
+							newaa = random.choice(aas);
+						for title in inseqs:
+							if inseqs[title][x] == r1:
+								pseqs[title] = pseqs[title] + newaa;
+							else:
+								pseqs[title] = pseqs[title] + inseqs[title][x];
+					else:
+						for title in inseqs:
 							pseqs[title] = pseqs[title] + inseqs[title][x];
-				else:
-					for title in inseqs:
-						pseqs[title] = pseqs[title] + inseqs[title][x];
-			inseqs = pseqs;
-		else:
-			for title in inseqs:
-				inseqs[title] = inseqs[title].replace(r1,r2);		
+				inseqs = pseqs;
+			else:
+				for title in inseqs:
+					inseqs[title] = inseqs[title].replace(r1,r2);		
 
 	for title in inseqs:
 	#Writes the sequences to the output file.
