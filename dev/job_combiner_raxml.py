1bd369b2cb30db3ea88e4a71514aa670671787cc
gwct <sonotiro@gmail.com>
2017-09-13 13:23:21 -0400

    added job scripts to dev

---
+++ dev/job_combiner_raxml.py
@@ -0,0 +1,116 @@
+import sys, os
+
+if len(sys.argv) not in [2,3] or not os.path.exists(os.path.abspath(sys.argv[1])) or (len(sys.argv) == 3 and sys.argv[2] != "1"):
+	print "Usage: $ python job_combiner_2.py [indir] [bootstrap flag = 1]";
+	sys.exit();
+
+indir = os.path.abspath(sys.argv[1]);
+bs_flag = False;
+if len(sys.argv) == 3:
+	if sys.argv[2] == "1":
+		bs_flag = True;
+
+outdir = os.path.join(indir, "raxml-combined/");
+rax_bestdir = os.path.join(outdir, "raxml-best/");
+rax_outdir = os.path.join(outdir, "raxml-out/");
+
+if not os.path.isdir(outdir):
+	print "+Creating output directory";
+	os.system("mkdir " + outdir);
+if not os.path.isdir(rax_outdir):
+	print "+Creating raxml-out directory";
+	os.system("mkdir " + rax_outdir);
+if not os.path.isdir(rax_bestdir):
+	print "+Creating raxml-best directory";
+	os.system("mkdir " + rax_bestdir);
+
+dirlist = os.listdir(indir);
+num_files = 0;
+tree_fail = [];
+trees = {};
+
+print "\n=======================================================================\n";
+for jobdir in dirlist:
+	if jobdir in [".DS_Store", "raxml-combined", "job_scripts", "job-scripts"] or ".sh" in jobdir:
+		continue;
+	fulldir = os.path.join(indir, jobdir);
+	print "Current job directory: " + fulldir;
+	fulllist = os.listdir(fulldir);
+
+	cur_num_files = len([f for f in fulllist if os.path.isfile(f)]);
+	print "# of files: ", cur_num_files;
+	num_files += cur_num_files;
+
+	dir_max = 0;
+	raxdir = "";
+	for adir in fulllist:
+		if "run-raxml" in adir:
+			dir_num = int(adir[adir.rindex("-")+1:]);
+			if dir_num > dir_max:
+				dir_max = dir_num;
+				raxdir = adir;
+	raxdir = os.path.join(fulldir, raxdir);
+	print "Current rax directory: " + raxdir;
+
+	cur_outdir = os.path.join(raxdir, "raxml-out/");
+	cur_bestdir = os.path.join(raxdir, "raxml-best/");
+	cur_treefile = os.path.join(raxdir, "best-trees.txt");
+
+	print "Getting trees....";
+	for line in open(cur_treefile):
+		line = line.strip().split("\t");
+		geneid = line[0];
+		tree = line[1];
+		trees[geneid] = tree;
+
+	print "Copying raxml output files...";
+	cp_cmd = "cp -r " + cur_outdir + "* " + rax_outdir;
+	print cp_cmd;
+	os.system(cp_cmd);
+
+	print "Copying raxml best trees...";
+	cp_cmd = cp_cmd = "cp " + cur_bestdir + "* " + rax_bestdir;
+	print cp_cmd;
+	os.system(cp_cmd);
+		
+	print "Checking log...";
+	wrapfiles = os.listdir(raxdir);
+	logfile = os.path.join(raxdir, [f for f in wrapfiles if '.log' in f][0]);
+	for line in open(logfile):
+		if "did not have trees made successfully" in line:
+			failed = line.split(": ")[1].split(",");
+			print "Following files failed:", ",".join(failed);
+			tree_fail += failed;
+
+	print "-----------------------------------------";
+
+print "Writing final tree files...";
+best_outfile = open(os.path.join(outdir, "best-trees.txt"), "w");
+astral_outfile = open(os.path.join(outdir, "gt-for-astral.txt"), "w");
+sdm_outfile = open(os.path.join(outdir, "gt-for-sdm.txt"), "w");
+sdm_outfile.write(str(len(trees)) + "\n");
+
+if bs_flag:
+	astral_bsfile = open(os.path.join(outdir, "bs-for-astral.txt"), "w");
+
+for geneid in trees:
+	tree = trees[geneid];
+	best_outfile.write(geneid + "\t" + tree + "\n");
+	astral_outfile.write(tree + "\n");
+	sdm_outfile.write(tree + "\n");
+
+	if bs_flag:
+		cur_outdir = os.path.join("raxml-out/", geneid + "-raxout");
+		cur_bsfile = os.path.join(cur_outdir, "RAxML_bootstrap." + geneid);
+		astral_bsfile.write(cur_bsfile + "\n");
+
+best_outfile.close();
+astral_outfile.close();
+sdm_outfile.close();
+if bs_flag:
+	astral_bsfile.close();
+print "Done!";
+print "-----";
+if tree_fail != []:
+	print "# The following", len(tree_fail), "file(s) did not have trees made successfully:" + ",".join(tree_fail);
+print "=======================================================================\n";
---
+++ dev/job_maker_raxml.py
@@ -0,0 +1,90 @@
+import sys, os
+
+# if len(sys.argv) != 6:
+# 	print "Usage: $ python job_maker.py [order] [# jobs] [time] [memory] [# ppn]";
+# 	sys.exit();
+
+# curorder = sys.argv[1];
+# jobs = int(sys.argv[2]);
+# time = sys.argv[3];
+# mem = sys.argv[4];
+# proc = sys.argv[5];
+
+if len(sys.argv) != 10 or "-h" in sys.argv:
+	print "Usage: $ python job_maker.py [order] [indir] [outdir] [# jobs] [time] [memory] [# ppn] [# bootstrap] [raxml model]";
+	sys.exit();
+
+curorder = sys.argv[1];
+indir = os.path.abspath(sys.argv[2]);
+outdir = os.path.abspath(sys.argv[3]);
+jobs = int(sys.argv[4]);
+time = sys.argv[5];
+mem = sys.argv[6];
+proc = sys.argv[7];
+bs = sys.argv[8];
+model = sys.argv[9];
+
+if not os.path.isdir(indir):
+	print "* ERROR: Specified input directory does not exist.";
+	sys.exit();
+if not os.path.isdir(outdir):
+	print "* MESSAGE: Specified output directory does not exist. Creating it for you.";
+	cmd = "mkdir " + outdir;
+	print cmd;
+	os.system(cmd);
+
+print "Splitting files and writing job scripts...";
+filelist = os.listdir(indir);
+chunksize = float(len(filelist))/jobs
+chunks = [ filelist [i:i + int(chunksize)] for i in range(0, (jobs-1)*int(chunksize), int(chunksize))]
+chunks.append(filelist[(jobs-1)*int(chunksize):])
+
+jobdir = os.path.join(outdir, "job-scripts");
+if not os.path.isdir(jobdir):
+	os.system("mkdir " + jobdir);
+
+jobfiles = [];
+chunk_num = 1;
+for chunk in chunks:
+	print "Chunk", chunk_num;
+	chunkdir = os.path.join(outdir, str(chunk_num));
+	if not os.path.isdir(chunkdir):
+		os.system("mkdir " + chunkdir);
+	chunkoutdir = os.path.join(chunkdir, "run-raxml");
+	for align in chunk:
+		infilename = os.path.join(indir, align);
+		outfilename = os.path.join(chunkdir, align);
+		os.system("cp " + infilename + " " + outfilename);
+	
+	jobfilename = os.path.join(jobdir, curorder + "-" + str(chunk_num) + ".pbs");
+	jobfiles.append(jobfilename);
+	jobfile = open(jobfilename, "w");
+	jobfile.write("#!/bin/bash\n");
+	jobfile.write("#PBS -k o\n");
+	jobfile.write("#PBS -l nodes=1:ppn=" + proc + ",walltime=" + time + ":00:00,vmem=" + mem + "GB\n");
+	jobfile.write("#PBS -M grthomas@indiana.edu\n");
+	jobfile.write("#PBS -m abe\n");
+	jobfile.write("#PBS -N " + curorder + "-" + str(chunk_num) + "\n");
+	jobfile.write("#PBS -j oe\n");
+	jobfile.write("#PBS -o /N/dc2/scratch/grthomas/i5k/\n");
+	jobfile.write("#PBS -d /N/dc2/scratch/grthomas/i5k/\n");
+
+	cmd = "time -p python /N/u/grthomas/Carbonate/bin/core/wrappers.py --raxml -i " + chunkdir + " -p /N/u/grthomas/Carbonate/bin/raxml/raxml-pthreads -model " + model + " ";
+	if bs != 0:
+		cmd += "-b " + bs + " ";
+	if proc != '1':
+		cmd += "-t " + str(int(proc)-1) + " ";
+	cmd += "-v 0 -o " + chunkoutdir;
+	jobfile.write(cmd);
+
+	jobfile.close();
+	chunk_num += 1;
+
+
+submitfilename = os.path.join(outdir, curorder + "-submit.sh");
+submitfile = open(submitfilename, "w");
+submitfile.write("#!/bin/bash\n");
+for jobfile in jobfiles:
+	submitfile.write("qsub " + jobfile + "\n");
+submitfile.close();
+
