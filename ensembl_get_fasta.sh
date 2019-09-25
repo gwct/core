b138c13ec55dd980b43d1c0fc4dbf8e042deeb02
gwct <greggwct@gmail.com>
2019-09-24 22:42:51 -0400

    added an automated rsync script to get fasta seqs from ensembl

---
+++ ensembl_get_fasta.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+############################################################
+# For sungorous project, 09.19
+# This takes a pseudo-it reference genome and intermediate
+# bam file and softmasks the reference genome (converts sites
+# with not enough info to call to lower case).
+############################################################
+
+display_usage() {
+# Usage message for -h
+    echo
+	echo "Usage:"
+    echo
+    echo "Read file directly:"
+    echo "ensembl_get_orths.sh -r [97] -f [file] -o [both] -h"
+    echo
+    echo "Read from STDIN:"
+    echo "cat [file with species names, one per line] | ensembl_get_orths.sh -r [97] -o [both] -h"
+    echo
+    echo "-r: Ensembl release. Default: 97"
+    echo "-f: An input file with one Ensembl species name per line. Can also be read from STDIN."
+    echo "-o: The type of sequence to retrieve: cds (coding sequences), pep (peptides), or both (default)."
+    echo "-h: Display this help message."
+    echo
+    echo "====================================="
+    exit 0
+}
+
+##########
+
+invalid_opt() {
+# Invalid input option message
+    echo "Error 1: Invalid option: $1"
+    echo "====================================="
+    exit 1
+}
+
+##########
+# Invalid output option message
+invalit_out(){
+    echo "Error 2: Invalid output (-o) option: $1"
+    echo "Error 2: Output (-o) must be one of: both, pep, cds"
+    echo "====================================="
+    exit 1   
+}
+
+############################################################
+
+echo
+echo "====================================="
+echo "Get Ensembl sequences with rsync for all species in a file."
+release="97"
+input="/dev/stdin"
+output="both"
+# Defaults
+
+while getopts ":r:f:o:h" arg; do
+    case $arg in
+        h) display_usage;;
+        r) release=$OPTARG;;
+        f) input=$OPTARG;;
+        o) output=$OPTARG;;
+        \?) invalid_opt $OPTARG ;;
+    esac
+done
+# Parse input options
+
+if [ $output != "both" ] && [ $output != "pep" ] && [ $output != "cds" ]; then
+    invalid_out $output
+fi
+# Check output option
+
+release="release-"$release
+echo "Reading species names from: " $input
+echo "Setting Ensembl release to: " $release
+echo "====================================="
+echo
+# Print info
+
+while read spec; do
+    echo $spec
+    mkdir $spec
+    #cd $spec
+    if [ $output == "cds" ] || [ $output == "both" ]; then
+        rsync -av rsync://ftp.ensembl.org/ensembl/pub/$release/fasta/$spec/cds/*.cds.all.fa.gz $spec/.
+        rsync -av rsync://ftp.ensembl.org/ensembl/pub/$release/fasta/$spec/cds/CHECKSUMS $spec/CHECKSUMS-CDS
+    fi
+
+    if [ $output == "cds" ] || [ $output == "both" ]; then
+        rsync -av rsync://ftp.ensembl.org/ensembl/pub/$release/fasta/$spec/pep/*.pep.all.fa.gz $spec/.
+        rsync -av rsync://ftp.ensembl.org/ensembl/pub/$release/fasta/$spec/pep/CHECKSUMS $spec/CHECKSUMS-PEP
+    fi   
+    #rsync -av rsync://ftp.ensembl.org/ensembl/pub/latest/fasta/$spec/cds/ $spec/
+    #rsync -av rsync://ftp.ensembl.org/ensembl/pub/latest/fasta/$spec/pep/ $spec/
+done < "$input"
+# Run rsync
+
+echo
+echo "Done!"
+echo "====================================="
